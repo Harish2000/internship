6d6f52bac0009986b8d474d21b6e3dabdc71af05
Harish2000 <68814467+Harish2000@users.noreply.github.com>
2020-07-28 15:42:48 +0530

    Update inter.py

--- inter.py
+++ inter.py
@@ -5,17 +5,33 @@ app = Flask(__name__)
 def login():
     username=request.json['username']
     password=request.json['password']
+    a="1234567890"
+    b="qwertyuiopasdfghjklzxcvbnm"
+    integer=0
+    character=0
+    for i in password:
+        if(i in a):
+            integer=1
+        elif i in b:
+            charater=1
+        if(integer==1 and character==1):
+            break
+    user_check=0
+    for i in username:
+        if i not in b:
+            user_check=1
+            break
     if(username=="vaibhav" and password=="abcd12"):
-        d={"msg":"Success","status":"200"}
+        d={"status":"200","msg":"Success"}
         return make_response(jsonify(d))
-    elif(username=="vaibhav" and password=="abcd"):
-        d={"msg":"Failure: password should be of length 6","status":"201"}
+    elif(len(password)<6):
+        d={"status":"201","msg":"Failure: password should be of length 6"}
         return make_response(jsonify(d))
-    elif(username=="vaibhav" and password=="abcdef"):
-        d={"msg":"Failure: password to have 1 character and 1 number","status":"202"}
+    elif(integer==0 or character==0):
+        d={"status":"202","msg":"Failure: password to have 1 character and 1 number"}
         return make_response(jsonify(d))
-    elif(username=="1234" and password=="abcd12"):
-        d={"msg":"Failure: only characters allowed in username","status":"203"}
+    elif(user_check==1):
+        d={"status":"203","msg":"Failure: only characters allowed in username"}
         return make_response(jsonify(d))
 if __name__ == '__main__':
     app.run(debug=True)
